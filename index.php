e5f73d23ecf7b0fc55f9dc63311f5c81e7b33bf0
SS-Haniya <cdmofficial.us@gmail.com>
2023-09-04 15:14:52 +0500

    twet

--- index.php
+++ index.php
@@ -114,10 +114,10 @@
 							<span style="background: linear-gradient(to right, #12CE5D 0%, #FFD80C 100%);-webkit-background-clip: text;-webkit-text-fill-color: transparent;">
 								<span id="kt_landing_hero_text">The Best Theme Ever</span>
 							</span></h1>
-       <div class="fs-5 text-muted fw-bold">Engage actively with Kakamega County's procurement and projects. Access transparent information, participate in procurement,<br> and track ongoing initiatives. Be a part of accountable development for the community.</div>
+       <div class="fs-5 text-white fw-bold">Engage actively with Kakamega County's procurement and projects. Access transparent information, participate in procurement,<br> and track ongoing initiatives. Be a part of accountable development for the community.</div>
 							<!--end::Title-->
 							<!--begin::Action-->
-							<a href="index.html" class="btn btn-primary m-12" style="background-color: #2c2617;">Learn More</a>
+							<a href="index.html" class="btn btn-primary m-12" style="background-color: #0a0a0a !important;">Learn More</a>
 							<!--end::Action-->
 						</div>
 						<!--end::Heading-->
@@ -148,7 +148,7 @@
 					</div>
 					<!--end::Heading-->
 					<!--begin::Row-->
-					<div class="row w-100 gy-10 mb-md-20">
+					<div class="row w-100 gy-10 mb-md-20 pb-20">
 						<!--begin::Col-->
 						<div class="col-md-4 px-5">
 							<!--begin::Story-->
@@ -224,58 +224,7 @@
 					</div>
 					<!--end::Row-->
 					<!--begin::Product slider-->
-					<div class="tns tns-default tns-initiazlied">
-						<!--begin::Slider-->
-						<div class="tns-outer" id="tns1-ow"><div class="tns-liveregion tns-visually-hidden" aria-live="polite" aria-atomic="true">slide <span class="current">4</span>  of 4</div><div id="tns1-mw" class="tns-ovh"><div class="tns-inner" id="tns1-iw"><div data-tns="true" data-tns-loop="true" data-tns-swipe-angle="false" data-tns-speed="2000" data-tns-autoplay="true" data-tns-autoplay-timeout="18000" data-tns-controls="true" data-tns-nav="false" data-tns-items="1" data-tns-center="false" data-tns-dots="false" data-tns-prev-button="#kt_team_slider_prev1" data-tns-next-button="#kt_team_slider_next1" class="  tns-slider tns-carousel tns-subpixel tns-calc tns-horizontal" id="tns1" style="transform: translate3d(-50%, 0px, 0px);"><div class="text-center px-5 pt-5 pt-lg-10 px-lg-10 tns-item tns-slide-cloned" aria-hidden="true" tabindex="-1">
-								<img src="assets/media/product-demos/demo5.png" class="card-rounded shadow mw-100" alt="">
-							</div>
-							<!--begin::Item-->
-							<div class="text-center px-5 pt-5 pt-lg-10 px-lg-10 tns-item" id="tns1-item0" aria-hidden="true" tabindex="-1">
-								<img src="assets/media/product-demos/demo1.png" class="card-rounded shadow mw-100" alt="">
-							</div>
-							<!--end::Item-->
-							<!--begin::Item-->
-							<div class="text-center px-5 pt-5 pt-lg-10 px-lg-10 tns-item" id="tns1-item1" aria-hidden="true" tabindex="-1">
-								<img src="assets/media/product-demos/demo2.png" class="card-rounded shadow mw-100" alt="">
-							</div>
-							<!--end::Item-->
-							<!--begin::Item-->
-							<div class="text-center px-5 pt-5 pt-lg-10 px-lg-10 tns-item tns-slide-active" id="tns1-item2">
-								<img src="assets/media/product-demos/demo4.png" class="card-rounded shadow mw-100" alt="">
-							</div>
-							<!--end::Item-->
-							<!--begin::Item-->
-							<div class="text-center px-5 pt-5 pt-lg-10 px-lg-10 tns-item" id="tns1-item3" aria-hidden="true" tabindex="-1">
-								<img src="assets/media/product-demos/demo5.png" class="card-rounded shadow mw-100" alt="">
-							</div>
-							<!--end::Item-->
-						<div class="text-center px-5 pt-5 pt-lg-10 px-lg-10 tns-item tns-slide-cloned" aria-hidden="true" tabindex="-1">
-								<img src="assets/media/product-demos/demo1.png" class="card-rounded shadow mw-100" alt="">
-							</div></div></div></div></div>
-						<!--end::Slider-->
-						<!--begin::Slider button-->
-						<button class="btn btn-icon btn-active-color-primary" id="kt_team_slider_prev1" aria-controls="tns1" tabindex="-1" data-controls="prev">
-							<!--begin::Svg Icon | path: icons/duotune/arrows/arr074.svg-->
-							<span class="svg-icon svg-icon-3x">
-								<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-									<path d="M11.2657 11.4343L15.45 7.25C15.8642 6.83579 15.8642 6.16421 15.45 5.75C15.0358 5.33579 14.3642 5.33579 13.95 5.75L8.40712 11.2929C8.01659 11.6834 8.01659 12.3166 8.40712 12.7071L13.95 18.25C14.3642 18.6642 15.0358 18.6642 15.45 18.25C15.8642 17.8358 15.8642 17.1642 15.45 16.75L11.2657 12.5657C10.9533 12.2533 10.9533 11.7467 11.2657 11.4343Z" fill="currentColor"></path>
-								</svg>
-							</span>
-							<!--end::Svg Icon-->
-						</button>
-						<!--end::Slider button-->
-						<!--begin::Slider button-->
-						<button class="btn btn-icon btn-active-color-primary" id="kt_team_slider_next1" aria-controls="tns1" tabindex="-1" data-controls="next">
-							<!--begin::Svg Icon | path: icons/duotune/arrows/arr071.svg-->
-							<span class="svg-icon svg-icon-3x">
-								<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-									<path d="M12.6343 12.5657L8.45001 16.75C8.0358 17.1642 8.0358 17.8358 8.45001 18.25C8.86423 18.6642 9.5358 18.6642 9.95001 18.25L15.4929 12.7071C15.8834 12.3166 15.8834 11.6834 15.4929 11.2929L9.95001 5.75C9.5358 5.33579 8.86423 5.33579 8.45001 5.75C8.0358 6.16421 8.0358 6.83579 8.45001 7.25L12.6343 11.4343C12.9467 11.7467 12.9467 12.2533 12.6343 12.5657Z" fill="currentColor"></path>
-								</svg>
-							</span>
-							<!--end::Svg Icon-->
-						</button>
-						<!--end::Slider button-->
-					</div>
+					
 					<!--end::Product slider-->
 				</div>
 				<!--end::Container-->
@@ -287,7 +236,7 @@
 				
 				<!--end::Curve top-->
 				<!--begin::Wrapper-->
-				<div class="pb-2 pt-18 landing-dark-bg">
+				<div class="pb-2 pt-18 landing-dark-bg" style="background-color: black !important;">
 					<!--begin::Container-->
 					<div class="container">
     <div class="row g-5 g-xl-10 mb-5 mb-xl-10">
@@ -338,172 +287,7 @@
 			</div>
 			<!--end::Statistics Section-->
 			<!--begin::Team Section-->
-			<div class="py-10 py-lg-20">
-				<!--begin::Container-->
-				<div class="container">
-					<!--begin::Heading-->
-					<div class="text-center mb-12">
-						<!--begin::Title-->
-						<h3 class="fs-2hx text-dark mb-5" id="team" data-kt-scroll-offset="{default: 100, lg: 150}">Our Great Team</h3>
-						<!--end::Title-->
-						<!--begin::Sub-title-->
-						<div class="fs-5 text-muted fw-bold">It’s no doubt that when a development takes longer to complete, additional costs to
-						<br />integrate and test each extra feature creeps up and haunts most of us.</div>
-						<!--end::Sub-title=-->
-					</div>
-					<!--end::Heading-->
-					<!--begin::Slider-->
-					<div class="tns tns-default">
-						<!--begin::Wrapper-->
-						<div data-tns="true" data-tns-loop="true" data-tns-swipe-angle="false" data-tns-speed="2000" data-tns-autoplay="true" data-tns-autoplay-timeout="18000" data-tns-controls="true" data-tns-nav="false" data-tns-items="1" data-tns-center="false" data-tns-dots="false" data-tns-prev-button="#kt_team_slider_prev" data-tns-next-button="#kt_team_slider_next" data-tns-responsive="{1200: {items: 3}, 992: {items: 2}}">
-							<!--begin::Item-->
-							<div class="text-center">
-								<!--begin::Photo-->
-								<div class="octagon mx-auto mb-5 d-flex w-200px h-200px bgi-no-repeat bgi-size-contain bgi-position-center" style="background-image:url('assets/media/avatars/300-1.jpg')"></div>
-								<!--end::Photo-->
-								<!--begin::Person-->
-								<div class="mb-0">
-									<!--begin::Name-->
-									<a href="#" class="text-dark fw-bolder text-hover-primary fs-3">Paul Miles</a>
-									<!--end::Name-->
-									<!--begin::Position-->
-									<div class="text-muted fs-6 fw-bold mt-1">Development Lead</div>
-									<!--begin::Position-->
-								</div>
-								<!--end::Person-->
-							</div>
-							<!--end::Item-->
-							<!--begin::Item-->
-							<div class="text-center">
-								<!--begin::Photo-->
-								<div class="octagon mx-auto mb-5 d-flex w-200px h-200px bgi-no-repeat bgi-size-contain bgi-position-center" style="background-image:url('assets/media/avatars/300-2.jpg')"></div>
-								<!--end::Photo-->
-								<!--begin::Person-->
-								<div class="mb-0">
-									<!--begin::Name-->
-									<a href="#" class="text-dark fw-bolder text-hover-primary fs-3">Melisa Marcus</a>
-									<!--end::Name-->
-									<!--begin::Position-->
-									<div class="text-muted fs-6 fw-bold mt-1">Creative Director</div>
-									<!--begin::Position-->
-								</div>
-								<!--end::Person-->
-							</div>
-							<!--end::Item-->
-							<!--begin::Item-->
-							<div class="text-center">
-								<!--begin::Photo-->
-								<div class="octagon mx-auto mb-5 d-flex w-200px h-200px bgi-no-repeat bgi-size-contain bgi-position-center" style="background-image:url('assets/media/avatars/300-5.jpg')"></div>
-								<!--end::Photo-->
-								<!--begin::Person-->
-								<div class="mb-0">
-									<!--begin::Name-->
-									<a href="#" class="text-dark fw-bolder text-hover-primary fs-3">David Nilson</a>
-									<!--end::Name-->
-									<!--begin::Position-->
-									<div class="text-muted fs-6 fw-bold mt-1">Python Expert</div>
-									<!--begin::Position-->
-								</div>
-								<!--end::Person-->
-							</div>
-							<!--end::Item-->
-							<!--begin::Item-->
-							<div class="text-center">
-								<!--begin::Photo-->
-								<div class="octagon mx-auto mb-5 d-flex w-200px h-200px bgi-no-repeat bgi-size-contain bgi-position-center" style="background-image:url('assets/media/avatars/300-20.jpg')"></div>
-								<!--end::Photo-->
-								<!--begin::Person-->
-								<div class="mb-0">
-									<!--begin::Name-->
-									<a href="#" class="text-dark fw-bolder text-hover-primary fs-3">Anne Clarc</a>
-									<!--end::Name-->
-									<!--begin::Position-->
-									<div class="text-muted fs-6 fw-bold mt-1">Project Manager</div>
-									<!--begin::Position-->
-								</div>
-								<!--end::Person-->
-							</div>
-							<!--end::Item-->
-							<!--begin::Item-->
-							<div class="text-center">
-								<!--begin::Photo-->
-								<div class="octagon mx-auto mb-5 d-flex w-200px h-200px bgi-no-repeat bgi-size-contain bgi-position-center" style="background-image:url('assets/media/avatars/300-23.jpg')"></div>
-								<!--end::Photo-->
-								<!--begin::Person-->
-								<div class="mb-0">
-									<!--begin::Name-->
-									<a href="#" class="text-dark fw-bolder text-hover-primary fs-3">Ricky Hunt</a>
-									<!--end::Name-->
-									<!--begin::Position-->
-									<div class="text-muted fs-6 fw-bold mt-1">Art Director</div>
-									<!--begin::Position-->
-								</div>
-								<!--end::Person-->
-							</div>
-							<!--end::Item-->
-							<!--begin::Item-->
-							<div class="text-center">
-								<!--begin::Photo-->
-								<div class="octagon mx-auto mb-5 d-flex w-200px h-200px bgi-no-repeat bgi-size-contain bgi-position-center" style="background-image:url('assets/media/avatars/300-12.jpg')"></div>
-								<!--end::Photo-->
-								<!--begin::Person-->
-								<div class="mb-0">
-									<!--begin::Name-->
-									<a href="#" class="text-dark fw-bolder text-hover-primary fs-3">Alice Wayde</a>
-									<!--end::Name-->
-									<!--begin::Position-->
-									<div class="text-muted fs-6 fw-bold mt-1">Marketing Manager</div>
-									<!--begin::Position-->
-								</div>
-								<!--end::Person-->
-							</div>
-							<!--end::Item-->
-							<!--begin::Item-->
-							<div class="text-center">
-								<!--begin::Photo-->
-								<div class="octagon mx-auto mb-5 d-flex w-200px h-200px bgi-no-repeat bgi-size-contain bgi-position-center" style="background-image:url('assets/media/avatars/300-9.jpg')"></div>
-								<!--end::Photo-->
-								<!--begin::Person-->
-								<div class="mb-0">
-									<!--begin::Name-->
-									<a href="#" class="text-dark fw-bolder text-hover-primary fs-3">Carles Puyol</a>
-									<!--end::Name-->
-									<!--begin::Position-->
-									<div class="text-muted fs-6 fw-bold mt-1">QA Managers</div>
-									<!--begin::Position-->
-								</div>
-								<!--end::Person-->
-							</div>
-							<!--end::Item-->
-						</div>
-						<!--end::Wrapper-->
-						<!--begin::Button-->
-						<button class="btn btn-icon btn-active-color-primary" id="kt_team_slider_prev">
-							<!--begin::Svg Icon | path: icons/duotune/arrows/arr074.svg-->
-							<span class="svg-icon svg-icon-3x">
-								<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-									<path d="M11.2657 11.4343L15.45 7.25C15.8642 6.83579 15.8642 6.16421 15.45 5.75C15.0358 5.33579 14.3642 5.33579 13.95 5.75L8.40712 11.2929C8.01659 11.6834 8.01659 12.3166 8.40712 12.7071L13.95 18.25C14.3642 18.6642 15.0358 18.6642 15.45 18.25C15.8642 17.8358 15.8642 17.1642 15.45 16.75L11.2657 12.5657C10.9533 12.2533 10.9533 11.7467 11.2657 11.4343Z" fill="currentColor" />
-								</svg>
-							</span>
-							<!--end::Svg Icon-->
-						</button>
-						<!--end::Button-->
-						<!--begin::Button-->
-						<button class="btn btn-icon btn-active-color-primary" id="kt_team_slider_next">
-							<!--begin::Svg Icon | path: icons/duotune/arrows/arr071.svg-->
-							<span class="svg-icon svg-icon-3x">
-								<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-									<path d="M12.6343 12.5657L8.45001 16.75C8.0358 17.1642 8.0358 17.8358 8.45001 18.25C8.86423 18.6642 9.5358 18.6642 9.95001 18.25L15.4929 12.7071C15.8834 12.3166 15.8834 11.6834 15.4929 11.2929L9.95001 5.75C9.5358 5.33579 8.86423 5.33579 8.45001 5.75C8.0358 6.16421 8.0358 6.83579 8.45001 7.25L12.6343 11.4343C12.9467 11.7467 12.9467 12.2533 12.6343 12.5657Z" fill="currentColor" />
-								</svg>
-							</span>
-							<!--end::Svg Icon-->
-						</button>
-						<!--end::Button-->
-					</div>
-					<!--end::Slider-->
-				</div>
-				<!--end::Container-->
-			</div>
+			
 			<!--end::Team Section-->
 			<!--begin::Projects Section-->
 			<div class="mb-lg-n15 position-relative z-index-2">
