8456f003996fd03831b748d162ea871d05153b2e
SS-Haniya <cdmofficial.us@gmail.com>
2023-09-04 15:26:54 +0500

    fewf

--- index.php
+++ index.php
@@ -768,27 +768,15 @@
 							<!--begin::Copyright-->
 							<div class="d-flex align-items-center order-2 order-md-1">
 								<!--begin::Logo-->
-								<a href="landing.html">
-									<img alt="Logo" src="assets/media/logos/logo-landing.svg" class="h-15px h-md-20px" />
-								</a>
+								
 								<!--end::Logo image-->
 								<!--begin::Logo image-->
-								<span class="mx-5 fs-6 fw-bold text-gray-600 pt-1" href="https://keenthemes.com/">© 2022 Keenthemes Inc.</span>
+								<span class="mx-5 fs-6 fw-bold text-gray-600 pt-1" href="https://techkira.net/">© 2023 KCTS.</span>
 								<!--end::Logo image-->
 							</div>
 							<!--end::Copyright-->
 							<!--begin::Menu-->
-							<ul class="menu menu-gray-600 menu-hover-primary fw-bold fs-6 fs-md-5 order-1 mb-5 mb-md-0">
-								<li class="menu-item">
-									<a href="https://keenthemes.com/" target="_blank" class="menu-link px-2">About</a>
-								</li>
-								<li class="menu-item mx-5">
-									<a href="https://devs.keenthemes.com/" target="_blank" class="menu-link px-2">Support</a>
-								</li>
-								<li class="menu-item">
-									<a href="#" target="_blank" class="menu-link px-2">Purchase</a>
-								</li>
-							</ul>
+							
 							<!--end::Menu-->
 						</div>
 						<!--end::Wrapper-->
