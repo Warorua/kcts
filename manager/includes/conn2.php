ed195c399d56dca516c633564c11df6ad8aef4ab
Warorua <waroruaalex640@gmail.com>
2023-09-08 13:42:56 +0300

    fgtjfgtjftyj

--- manager/includes/conn2.php
+++ manager/includes/conn2.php
@@ -1,5 +1,34 @@
 <?php
+/*
+class Database
+{
 
+	private $server = "mysql:host=45.84.206.68;dbname=tsavosit_kakamega";
+	private $username = "tsavosit_kakamega";
+	private $password = "zSC1OL)*R(ln";
+	private $options  = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,);
+	protected $conn;
+
+	public function open()
+	{
+		try {
+			$this->conn = new PDO($this->server, $this->username, $this->password, $this->options);
+			return $this->conn;
+		} catch (PDOException $e) {
+			echo "There is some problem in connection: " . $e->getMessage();
+		}
+	}
+
+	public function close()
+	{
+		$this->conn = null;
+	}
+}
+
+//*/
+
+
+//*
 if (!class_exists('Database')) {
 	// Define your own Database class if it's not already defined
 	class Database
@@ -27,6 +56,7 @@ if (!class_exists('Database')) {
 		}
 	}
 }
+//*/
 
 $pdo = new Database();
 
