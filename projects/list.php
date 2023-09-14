f56f9195eac3b393929ba08c6f3c6ff995bfd3e0
SS-Haniya <cdmofficial.us@gmail.com>
2023-09-14 23:42:02 +0500

    search

--- projects/list.php
+++ projects/list.php
@@ -152,7 +152,7 @@ include '../includes/head.php';
 							<div class="d-flex flex-wrap flex-stack my-5">
 								<!--begin::Heading-->
 								<h2 class="fs-2 fw-bold my-2">Projects 
-								<span class="fs-6 text-gray-400 ms-1">Lurambi</span></h2>
+								<span class="fs-6 text-gray-400 ms-1">All</span></h2>
 								<!--end::Heading-->
 								<!--begin::Controls-->
 								<div class="d-flex flex-wrap my-1">
@@ -172,6 +172,57 @@ include '../includes/head.php';
 								<!--end::Controls-->
 							</div>
 							<!--end::Toolbar-->
+
+							<!-- begin search --->
+							<form action="#">
+								<!--begin::Card-->
+								<div class="card mb-7">
+									<!--begin::Card body-->
+									<div class="card-body">
+										<!--begin::Compact form-->
+										<div class="d-flex align-items-center">
+											<!--begin::Input group-->
+											
+											<div class="position-relative w-md-400px me-md-2">
+												<!--begin::Svg Icon | path: icons/duotune/general/gen021.svg-->
+												<span class="svg-icon svg-icon-3 svg-icon-gray-500 position-absolute top-50 translate-middle ms-6">
+													<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+														<rect opacity="0.5" x="17.0365" y="15.1223" width="8.15546" height="2" rx="1" transform="rotate(45 17.0365 15.1223)" fill="currentColor"></rect>
+														<path d="M11 19C6.55556 19 3 15.4444 3 11C3 6.55556 6.55556 3 11 3C15.4444 3 19 6.55556 19 11C19 15.4444 15.4444 19 11 19ZM11 5C7.53333 5 5 7.53333 5 11C5 14.4667 7.53333 17 11 17C14.4667 17 17 14.4667 17 11C17 7.53333 14.4667 5 11 5Z" fill="currentColor"></path>
+													</svg>
+												</span>
+												<!--end::Svg Icon-->
+												<input type="text" class="form-control form-control-solid ps-10" name="search" value="" placeholder="Search Department">
+											</div><div class="position-relative w-md-400px me-md-2">
+												<!--begin::Svg Icon | path: icons/duotune/general/gen021.svg-->
+												<span class="svg-icon svg-icon-3 svg-icon-gray-500 position-absolute top-50 translate-middle ms-6">
+													<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+														<rect opacity="0.5" x="17.0365" y="15.1223" width="8.15546" height="2" rx="1" transform="rotate(45 17.0365 15.1223)" fill="currentColor"></rect>
+														<path d="M11 19C6.55556 19 3 15.4444 3 11C3 6.55556 6.55556 3 11 3C15.4444 3 19 6.55556 19 11C19 15.4444 15.4444 19 11 19ZM11 5C7.53333 5 5 7.53333 5 11C5 14.4667 7.53333 17 11 17C14.4667 17 17 14.4667 17 11C17 7.53333 14.4667 5 11 5Z" fill="currentColor"></path>
+													</svg>
+												</span>
+												<!--end::Svg Icon-->
+												<input type="text" class="form-control form-control-solid ps-10" name="search" value="" placeholder="Search County">
+											</div>
+											<!--end::Input group-->
+											<!--begin:Action-->
+											<div class="d-flex align-items-center">
+												<button type="submit" class="btn btn-primary me-5">Search</button>
+												
+											</div>
+											<!--end:Action-->
+										</div>
+										<!--end::Compact form-->
+										<!--begin::Advance form-->
+										
+										<!--end::Advance form-->
+									</div>
+									<!--end::Card body-->
+								</div>
+								<!--end::Card-->
+							</form>
+							<!-- end search --->
+
 							<!--begin::Row-->
 							<div class="row g-6 g-xl-9">
 								<?php
